b0ad432e8ddc88626443f98783ff2824d64a6f47
gurubac <gbrbachchu@gmail.com>
2021-07-04 08:50:49 -0700

    server working

--- src/server.js
+++ src/server.js
@@ -1,1 +1,12 @@
-console.log("hi")
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(PORT, () => {
+  console.log(`Example app listening at http://localhost:${PORT}`)
+})
