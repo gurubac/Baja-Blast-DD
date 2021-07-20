75b2c4e22ce43bcdf77bce3a3f7069457d0c2209
gurubac <gbrbachchu@gmail.com>
2021-07-19 18:47:08 -0700

    added todolist

--- User.js
+++ User.js
@@ -9,14 +9,14 @@ const User = require("./models/Location");
 //     });
 // }
 
-async function createUser() {
-    const newUser = await User.create({
-        address: "811 Kifer Rd",
-        city: "Sunnyvale",
-        state: "CA",
-        zip: "94086"
-    });
-}
+// async function createUser() {
+//     const newUser = await User.create({
+//         address: "811 Kifer Rd",
+//         city: "Sunnyvale",
+//         state: "CA",
+//         zip: "94086"
+//     });
+// }
 console.log("Starting User.js");
 
 createUser();
