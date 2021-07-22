05ae91e818a51a3b142cb9dcdaad08fe509360f4
gurubac <gbrbachchu@gmail.com>
2021-07-22 15:28:35 -0700

    added address tagline and another route for 2nd one

--- src/server.js
+++ src/server.js
@@ -1,51 +1,60 @@
-require('dotenv').config();
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
 const app = express();
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const PORT = process.env.PORT;
-const signale = require('signale');
-const Location = require('../models/Location');
+const signale = require("signale");
+const Location = require("../models/Location");
 //const User = require('../User');
 
 mongoURL = process.env.MONGO_URL;
 //connect to mongodb
-mongoose.connect(mongoURL, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useFindAndModify: false,
-        useCreateIndex: true,
-      })
-      .then(() => {
-        signale.success("Database Connected!");
-      })
-      .catch((err) => {
-        signale.error(err);
-      });
-app.use(express.static("public")); 
-app.set('view engine','ejs')
-
-app.get('/', (req, res) => {
-  Location.find({}, function(err, locations) {
-    //console.log(location);
-    res.render('index', {
-      locationList : locations
-    })
+mongoose
+  .connect(mongoURL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
   })
-})
-async function findUser(address){
+  .then(() => {
+    signale.success("Database Connected!");
+  })
+  .catch((err) => {
+    signale.error(err);
+  });
+
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  Location.find({}, function (err, locations) {
+    //console.log(location);
+    res.render("index", {
+      locationList: locations,
+    });
+  });
+});
+
+async function findUser(address) {
   return Location.findOne({ address: address });
 }
 
+app.get("/2779%20Aborn%20Rd", async (req, res) => {
+  let data = await findUser("2779 Aborn Rd");
+  //console.log(data);
+  res.render("results", {
+    location: data,
+  });
+});
 
-app.get('/2779%20Aborn%20Rd', async (req, res) => {
-  
-  let data = await findUser('2779 Aborn Rd');
-    //console.log(data);
-   res.render('results')
-  })
+app.get("/811%20Kifer%20Rd", async (req, res) => {
+  let data = await findUser("811 Kifer Rd");
+  //console.log(data);
+  res.render("results", {
+    location: data,
+  });
+});
 
 app.listen(PORT, () => {
-  console.log(`Deployed at http://localhost:${PORT}`)
-})
-
-
+  console.log(`Deployed at http://localhost:${PORT}`);
+});
